5dcaf3094deb1a733c8cb93529694962af93217a
Perriguey <lperrigu.student.42.fr>
2021-01-28 22:25:10 +0100

    start implementing easy computer functions

--- computer_play.py
+++ computer_play.py
@@ -13,6 +13,35 @@
 import sys
 from tools import played
 
+def safe_discard(deck, hands, board_state, tokens, current_player):
+    hands[current_player][0][0] = 3
+    hands[current_player][0][1] = 'r'
+    hands[current_player][0][2] = True
+    hands[current_player][0][3] = True
+    board_state['r'] = 2
+    for i in range(len(hands[current_player])):
+        number = hands[current_player][i][0]
+        color = hands[current_player][i][1]
+        knowed = hands[current_player][i][2] and hands[current_player][i][3]
+        if board_state[color] >= number and knowed:
+            discarded(i, deck, hands, board_state, tokens, current_player)
+            return True
+    return False
+
+
+def play_something(deck, hands, board_state, tokens, current_player):
+    print(hands[current_player])
+    print(board_state)
+    for i in range(len(hands[current_player])):
+        number = hands[current_player][i][0]
+        color = hands[current_player][i][1]
+        knowed = hands[current_player][i][2] and hands[current_player][i][3]
+        if board_state[color] == number - 1 and knowed:
+            played(i, deck, hands, board_state, tokens, current_player)
+            return True
+    return False
+
+
 def play_a_five(deck, hands, board_state, tokens, current_player):
     #hands[current_player][0][0] = 5
     #hands[current_player][0][1] = 'w'
@@ -32,13 +61,12 @@ def play_a_five(deck, hands, board_state, tokens, current_player):
 
 
 def computer_play(deck, hands, board_state, tokens, current_player):
-    #obvious_play(deck, hands, board_state, tokens, current_player)
     if play_a_five(deck, hands, board_state, tokens, current_player):
         return
     if play_something(deck, hands, board_state, tokens, current_player):
         return
     if safe_discard(deck, hands, board_state, tokens, current_player):
         return
-    if give_hint(deck, hands, board_state, tokens, current_player):
-        return
-    yolo_discard(deck, hands, board_state, tokens, current_player)
+    #if give_hint(deck, hands, board_state, tokens, current_player):
+    #    return
+    #yolo_discard(deck, hands, board_state, tokens, current_player)
