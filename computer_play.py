f21cfc06bfdd62edc5aa2391311e1731700820c5
Perriguey <lperrigu.student.42.fr>
2021-01-31 04:19:51 +0100

    starting hint computer func

--- computer_play.py
+++ computer_play.py
@@ -13,6 +13,21 @@
 import sys
 from tools import played
 
+def give_hint(deck, hands, board_state, tokens, current_player):
+    if token['hint'] == 0:
+        return False
+    possible_play = []
+    for player_i in len(hands):
+        if player_i != current_player:
+            for i in range(len(hands[player_i])):
+                number = hands[player_i][i][0]
+                color = hands[player_i][i][1]
+                unknowed = not hands[player_i][i][2] and not hands[player_i][i][3]
+                # Compter le nombre de carte hintee
+                # privilegier celle avec le max de jouable sans ambiguite
+                if board_state[color] == hands
+
+
 def safe_discard(deck, hands, board_state, tokens, current_player):
     hands[current_player][0][0] = 3
     hands[current_player][0][1] = 'r'
